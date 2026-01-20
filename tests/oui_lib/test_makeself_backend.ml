9ebeacf77e7e33014088517506791468b9336a53
Raja Boujbel <rjbou@ocamlpro.com>
2026-01-20 17:53:51 +0100

    makeself: add usage to install script

--- tests/oui_lib/test_makeself_backend.ml
+++ tests/oui_lib/test_makeself_backend.ml
@@ -82,6 +82,11 @@ let%expect_test "install_script: simple" =
     set -e
     PREFIX="/opt"
     BINPREFIX="/usr/local"
+    usage() {
+      echo "Ocaml Universal Installer for aaa.x.y.z"
+      echo ""
+      echo "Options:"
+    }
     check_available() {
       if [ -e "$1" ]; then
         printf '%s\n' "$1 already exists on the system! Aborting" >&2
@@ -160,6 +165,11 @@ let%expect_test "install_script: plugin_dirs dumped in install.conf" =
     set -e
     PREFIX="/opt"
     BINPREFIX="/usr/local"
+    usage() {
+      echo "Ocaml Universal Installer for t-name.t.version"
+      echo ""
+      echo "Options:"
+    }
     check_available() {
       if [ -e "$1" ]; then
         printf '%s\n' "$1 already exists on the system! Aborting" >&2
@@ -237,6 +247,11 @@ let%expect_test "install_script: install plugins" =
     set -e
     PREFIX="/opt"
     BINPREFIX="/usr/local"
+    usage() {
+      echo "Ocaml Universal Installer for t-name.t.version"
+      echo ""
+      echo "Options:"
+    }
     check_available() {
       if [ -e "$1" ]; then
         printf '%s\n' "$1 already exists on the system! Aborting" >&2
@@ -556,6 +571,11 @@ let%expect_test "install_script: binary in sub folder" =
     set -e
     PREFIX="/opt"
     BINPREFIX="/usr/local"
+    usage() {
+      echo "Ocaml Universal Installer for test-name.test.version"
+      echo ""
+      echo "Options:"
+    }
     check_available() {
       if [ -e "$1" ]; then
         printf '%s\n' "$1 already exists on the system! Aborting" >&2
@@ -664,6 +684,11 @@ let%expect_test "install_script: set environment for binaries" =
     set -e
     PREFIX="/opt"
     BINPREFIX="/usr/local"
+    usage() {
+      echo "Ocaml Universal Installer for test-name.test.version"
+      echo ""
+      echo "Options:"
+    }
     check_available() {
       if [ -e "$1" ]; then
         printf '%s\n' "$1 already exists on the system! Aborting" >&2
