3146c5ea192373d8c1b61345c71e930c501d792a
Nathan Rebours <nathan.rebours@ocamlpro.com>
2026-03-12 09:50:39 +0100

    Update copyright
    Signed-off-by: Nathan Rebours <nathan.rebours@ocamlpro.com>

--- src/oui_lib/system.ml
+++ src/oui_lib/system.ml
@@ -1,6 +1,6 @@
 (**************************************************************************)
 (*                                                                        *)
-(*    Copyright 2023 OCamlPro                                             *)
+(*    Copyright 2026 OCamlPro                                             *)
 (*                                                                        *)
 (*  All rights reserved. This file is distributed under the terms of the  *)
 (*  GNU Lesser General Public License version 2.1, with the special       *)
